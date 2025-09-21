9b809d37099fc4c41c86e9631fbe61450e8f91c1
CXwinter <CXknife@qq.com>
2025-09-21 13:10:37 +0800

    测试提交

--- Assets/Scripts/tes.cs
+++ Assets/Scripts/tes.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testsub : MonoBehaviour
+public class tes : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
